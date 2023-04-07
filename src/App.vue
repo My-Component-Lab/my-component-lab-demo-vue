72b8f5f83b840b1a6470a930682eb107a4a2f59a
Silas Huereca <silashuereca.dev@gmail.com>
2023-04-06 21:20:00 -0700

    feat: add buttons to project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,59 @@
 <template>
-    <div>Body</div>
+<MConfigProvider :schemas="schemas">
+    <h1>My Component Lab Demo</h1>
+    <div class="components-container">
+        <div class="buttons-container">
+            <MButton config-name="primaryButtonOne">Button</MButton>
+            <MButton config-name="primaryButtonTwo">Button</MButton>
+            <MButton config-name="primaryButtonThree">Button</MButton>
+            <MButton config-name="primaryButtonFour">Button</MButton>
+        </div>
+
+        <div class="buttons-container">
+            <MButton config-name="secondaryButton">Button</MButton>
+            <MButton config-name="secondaryButtonTwo">Button</MButton>
+            <MButton config-name="secondaryButtonThree">Button</MButton>
+            <MButton config-name="secondaryButtonFour">Button</MButton>
+        </div>
+    </div>
+</MConfigProvider>
 </template>
 
 
-<script >
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { MConfigProvider, MButton } from "my-component-lab";
+import schemas from "../my-component-schema.config.json";
+export default defineComponent({
+    components: {
+        MConfigProvider,
+        MButton
+    },
+    setup() {
+        return {
+            schemas
+        }
+    }
+})
 </script>
 
 <style scoped>
+h1 {
+    font-size: 3rem;
+    color: #FFF;
+    text-align: center;
+    margin-bottom: 3rem;
+    margin-top: 5rem;
+}
+.components-container {
+    max-width: 1200px;
+    margin: auto;
+}
+.buttons-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 20px;
+    gap: 20px;
+}
 </style>
